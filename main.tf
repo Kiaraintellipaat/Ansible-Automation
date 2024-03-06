2be4de6a9582a1156b91b76a6c9de9a257abf36d
Kiara <145350450+Kiaraintellipaat@users.noreply.github.com>
2024-03-06 18:25:58 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+        region = "us-east-2"
+        access_key = "AKIARZYTZU2OB2JT5REM"
+        secret_key = "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"
+}
+
+resource "aws_instance" "assignment-1" {
+        ami = "ami-0b4750268a88e78e0"
+        instance_type = "t2.micro"
+        key_name = "Ansible"
+        tags = {
+        Name = "assignment-1"
+        }
+}
